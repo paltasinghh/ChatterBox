8919697fb8efbe68074fd1fa67f1bd141d3d7a33
TestHabitatPlush <hptestplatform@gmail.com>
2025-10-25 11:37:06 +0530

    Initial commit - added frontend and backend

---
+++ backend/config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+const connectDB = async () => {
+  try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is undefined. Check .env file");
+    }
+
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.bold);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
